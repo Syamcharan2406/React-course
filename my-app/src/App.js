c5d28bef747444aa47f28427b56f43a85e3aa02c
Syamcharan2406 <144098199+Syamcharan2406@users.noreply.github.com>
2024-04-23 12:30:52 +0530

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import './components/website.css';
+
+
+function App() {
+  return (
+    <div className='.container'>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+
+
+export default App;
